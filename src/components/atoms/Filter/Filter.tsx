d4ecb2dbbdfcb872a09a3b87240054efa3994e68
kailina1001 <warvara1001@mail.ru>
2021-08-27 15:09:00 +0300

    add filter state

--- src/components/atoms/Filter/Filter.tsx
+++ src/components/atoms/Filter/Filter.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 import { memo } from "react";
 import "./index.css";
 
-export const Filter = memo(() => {
-  return <button className="filter-btn"></button>;
+interface IFilter {
+  onClickFilterBtn: () => void;
+}
+
+export const Filter = memo(({ onClickFilterBtn }: IFilter) => {
+  return <button className="btn__filter" onClick={onClickFilterBtn}></button>;
 });
